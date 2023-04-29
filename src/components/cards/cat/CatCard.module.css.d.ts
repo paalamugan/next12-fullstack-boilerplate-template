2c7819c2745a81c5d135061e338e5b056c833cef
paalamugan <paalamugan@gmail.com>
2023-04-29 20:14:51 +0530

    style: 💄 css module style declaration generator

--- src/components/cards/cat/CatCard.module.css.d.ts
+++ src/components/cards/cat/CatCard.module.css.d.ts
@@ -1,14 +1,15 @@
 declare const styles: {
-  readonly container: string;
-  readonly card__image: string;
-  readonly card__header: string;
-  readonly card: string;
-  readonly card__body: string;
-  readonly tag: string;
-  readonly 'tag-blue': string;
-  readonly card__footer: string;
-  readonly user: string;
-  readonly user__image: string;
-  readonly user__info: string;
+  readonly "container": string;
+  readonly "card__image": string;
+  readonly "card__header": string;
+  readonly "card": string;
+  readonly "card__body": string;
+  readonly "tag": string;
+  readonly "tag-blue": string;
+  readonly "card__footer": string;
+  readonly "user": string;
+  readonly "user__image": string;
+  readonly "user__info": string;
 };
 export = styles;
+
--- src/components/utility/search/Search.module.css.d.ts
+++ src/components/utility/search/Search.module.css.d.ts
@@ -1,4 +1,5 @@
 declare const styles: {
-  readonly component: string;
+  readonly "component": string;
 };
 export = styles;
+
